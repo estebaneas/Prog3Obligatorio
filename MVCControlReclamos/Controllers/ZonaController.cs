1de1e562e5a06dc1e3eb85f9c8112cb583759d1b
Emiliano Alvez <esteban.emiliano.alvez.saez@gmail.com>
2021-06-22 16:12:30 -0300

    Agregar mapa a mvc

---
+++ MVCControlReclamos/Controllers/ZonaController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCControlReclamos.Controllers
+{
+    public class ZonaController : Controller
+    {
+        // GET: Zona
+        public ActionResult Zona()
+        {
+            return View();
+        }
+    }
+}
