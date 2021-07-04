2c4eed91c7a08e79dd2e7d41dde2a5e5ff8a21cb
José Ramos <josecito598@hotmail.com>
2021-07-04 20:46:49 -0300

    Base Login

---
+++ MVCControlReclamos/Helpers/UserAuthentication.cs
@@ -0,0 +1,49 @@
+﻿using Common.Constantes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCControlReclamos.Helpers
+{
+    public class UserAuthentication : AuthorizeAttribute
+    {
+        protected override bool AuthorizeCore(HttpContextBase httpContext)
+        {
+
+            bool result = base.AuthorizeCore(httpContext);
+
+            if (result == false)
+            {
+                return result;
+            }
+
+            string tipoUsuario = (string)httpContext.Session[CLogin.KEY_SESSION_TIPO_USER];
+
+            //Verifico si la url tiene al menos un controller y una acción ese controller 
+            if (httpContext.Request.CurrentExecutionFilePath.Split('/').Length > 2)
+            {
+
+                string controller = httpContext.Request.CurrentExecutionFilePath.Split('/')[1];
+                string action = httpContext.Request.CurrentExecutionFilePath.Split('/')[2];
+
+                if (tipoUsuario == "2")
+                {
+                    if (controller == "Pelicula" && action == "Agregar")
+                    {
+                        return false;
+                    }
+
+                    if (controller == "Pelicula" && action == "Lista")
+                    {
+                        return true;
+                    }
+                }
+
+            }
+
+            return true;
+        }
+    }
+}
