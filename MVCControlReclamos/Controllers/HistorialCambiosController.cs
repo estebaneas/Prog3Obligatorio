a5a7254a51c98390c566075e416e913ac5075dc0
José Ramos <josecito598@hotmail.com>
2021-07-04 19:51:26 -0300

    View HistorialCambios

---
+++ MVCControlReclamos/Controllers/HistorialCambiosController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCControlReclamos.Controllers
+{
+    public class HistorialCambiosController : Controller
+    {
+        // GET: HistorialCambios
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
