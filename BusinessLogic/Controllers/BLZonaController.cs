5e915e7ea689910559488fe6295e4809ab78c850
Emiliano Alvez <esteban.emiliano.alvez.saez@gmail.com>
2021-07-05 02:57:04 -0300

    Agregado de proyecto Cliente para el lado del cliente adaptacion del mapa para el uso del cliente a la hora de realizar un reclamo,
    Mas trabajo en el mapa para la parte de alcaldía

--- BusinessLogic/Controllers/BLZonaController.cs
+++ BusinessLogic/Controllers/BLZonaController.cs
@@ -57,8 +57,6 @@ namespace BusinessLogic.Controllers
             return this._Repository.GetZonaRepository().listarZonas();
         }
 
-
-
         //validacion
         public List<string> validarZona(DtoZona dtoZona, bool modificacion)
         {
@@ -83,6 +81,172 @@ namespace BusinessLogic.Controllers
 
 
             return colErrores;
+        }
+
+        public bool puntoEnZonas(DtoPunto punto,List<DtoZona> zonas)
+        {
+            foreach (DtoZona zona in zonas)
+            {
+                if (puntoEnZona((float)punto.longitud, (float)punto.latitud, zona.colDtoPunto))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+
+        public bool zonaSuperPuesta(List<DtoPunto> newZonaPts, List<DtoZona> zonas)
+        {
+            for (int punto = 0; punto < newZonaPts.Count(); punto++)
+            {
+                DtoPunto pB1 = newZonaPts[punto];
+                DtoPunto pB2 = new DtoPunto();
+                if (newZonaPts.Count()-1>punto)
+                {
+                    pB2 = newZonaPts[punto + 1];
+                }else
+                {
+                    pB2 = newZonaPts[0];
+                }
+                
+                foreach (DtoZona zona in zonas)
+                {
+                    for (int i = 0; i < zona.colDtoPunto.Count(); i++)
+                    {
+                        DtoPunto pA1 = zona.colDtoPunto[i];
+                        DtoPunto pA2 = new DtoPunto();
+                        if (zona.colDtoPunto.Count()-1>i)
+                        {
+                            pA2 = zona.colDtoPunto[i + 1];
+                        }
+                        else
+                        {
+                            pA2 = zona.colDtoPunto[0];
+                        }
+
+                        if (vectoresCruzados(pA1, pA2, pB1, pB2))
+                        {
+                            return true;
+                        }
+                    }
+                }
+            }
+
+            return false;
+        }
+
+
+
+
+        //-------------------------------------------------------------------------------------------------
+
+        //Comprobar si las 2 lineas a comparar se cruzan
+        public static bool vectoresCruzados(DtoPunto pA1, DtoPunto pA2, DtoPunto pB1, DtoPunto pB2)
+        {
+            List<decimal> ABCP1 = pMatrizABC(pA1, pA2);
+            List<decimal> ABCP2 = pMatrizABC(pB1, pB2);
+            decimal A1 = ABCP1[0], B1 = ABCP1[1], C1 = ABCP1[2];
+            decimal A2 = ABCP2[0], B2 = ABCP2[1], C2 = ABCP2[2];
+            decimal det = A1 * B2 - A2 * B1;
+
+            if (det == 0)
+            {
+                return false;
+            }
+            else
+            {
+                decimal X = (B2 * C1 - B1 * C2) / det;
+                decimal Y = (A1 * C2 - A2 * C1) / det;
+                if (puntoEnVector(pA1, pA2, X, Y) && puntoEnVector(pB1, pB2, X, Y))
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+        public static List<decimal> pMatrizABC(DtoPunto a, DtoPunto b)
+        {
+            decimal x1 = a.latitud, x2 = b.latitud, y1 = a.longitud, y2 = b.longitud;
+            List<decimal> ABC = new List<decimal>();
+            decimal A = y2 - y1, B = x1 - x2, C = (A * x1) + (B * y1);
+            ABC.Add(A); ABC.Add(B); ABC.Add(C);
+            return ABC;
+        }
+        public static bool puntoEnVector(DtoPunto a, DtoPunto b, decimal X, decimal Y)
+        {
+            decimal x1 = a.latitud, x2 = b.latitud, y1 = a.longitud, y2 = b.longitud;
+            decimal minX = Math.Min(x1, x2);
+            decimal maxX = Math.Max(x1, x2);
+            decimal maxY = Math.Max(y1, y2);
+            decimal minY = Math.Min(y1, y2);
+
+            if (minX <= X && X <= maxX && minY <= Y && Y <= maxY)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    
+
+           //-------------------------------------------------------------------------------------------------
+
+        //calculos del punto en poligono
+        private static double DotProduct(double Ax, double Ay,double Bx, double By, double Cx, double Cy)
+        {
+            // Get the vectors' coordinates.
+            double BAx = Ax - Bx;
+            double BAy = Ay - By;
+            double BCx = Cx - Bx;
+            double BCy = Cy - By;
+
+            // Calculate the dot product.
+            return (BAx * BCx + BAy * BCy);
+        }
+
+        public static double CrossProductLength(double Ax, double Ay, double Bx, double By, double Cx, double Cy)
+        {
+            // Get the vectors' coordinates.
+            double BAx = Ax - Bx;
+            double BAy = Ay - By;
+            double BCx = Cx - Bx;
+            double BCy = Cy - By;
+
+            // Calculate the Z coordinate of the cross product.
+            return (BAx * BCy - BAy * BCx);
+        }
+
+        public static double GetAngle(double Ax, double Ay,double Bx, double By, double Cx, double Cy)
+        {
+            // Get the dot product.
+            double dot_product = DotProduct(Ax, Ay, Bx, By, Cx, Cy);
+
+            // Get the cross product.
+            double cross_product = CrossProductLength(Ax, Ay, Bx, By, Cx, Cy);
+
+            // Calculate the angle.
+            return (float)Math.Atan2(cross_product, dot_product);
+        }
+
+
+        public bool puntoEnZona(double X, double Y,List<DtoPunto> puntos)
+        {
+            int max_point = puntos.Count - 1;
+            double total_angle = GetAngle((double)puntos[max_point].latitud, (double)puntos[max_point].longitud,X, Y,(double)puntos[0].latitud, (double)puntos[0].longitud);
+            for (int i = 0; i < max_point; i++)
+            {
+                total_angle += GetAngle(
+                    (double)puntos[i].latitud, (double)puntos[i].longitud,
+                    X, Y,
+                    (double)puntos[i + 1].latitud, (double)puntos[i + 1].longitud);
+            }
+            return (Math.Abs(total_angle) > 1);
         }
 
     }
---
+++ MVCCliente/Controllers/UsuarioController.cs
@@ -0,0 +1,37 @@
+﻿using BusinessLogic.Controllers;
+using Common.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCCliente.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        // GET: Usuario
+        [HttpGet]
+        public ActionResult cargarZonasBD()
+        {
+            BLZonaController BLZ = new BLZonaController();
+            List<DtoZona> colZonas = BLZ.listarZonas();
+            return Json(colZonas, JsonRequestBehavior.AllowGet);
+        }
+
+
+       /*public ActionResult puntoEnZona (DtoPunto punto)
+        {
+            BLZonaController BLZ = new BLZonaController();
+            List<DtoZona> colZonas = BLZ.listarZonas();
+            bool puntoEnZ = BLZ.puntoEnZonas(punto, colZonas);
+
+            return Json()
+        }*/
+
+        public ActionResult Prueba()
+        {
+            return View();
+        }
+    }
+}
